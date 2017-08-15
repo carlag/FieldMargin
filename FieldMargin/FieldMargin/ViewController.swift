2bb76f5586cf0bd542e1a2372ccc06295880391c
Carla <carla.goldstein@gmail.com>
2017-08-15 11:29:10 +0100

    added return key to move to next textfield

--- FieldMargin/FieldMargin/ViewController.swift
+++ FieldMargin/FieldMargin/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var robotStartCoordinates_x: UITextField!
     @IBOutlet weak var robotStartCoordinates_y: UITextField!
 
-    @IBOutlet weak var listOfInstructions: UITextField!
-    
     @IBOutlet weak var listOfCrates: UITextField!
+
+    @IBOutlet weak var listOfInstructions: UITextField!    
     
     @IBOutlet weak var finalHealthStatus: UILabel!
     @IBOutlet weak var finalBagCount: UILabel!
@@ -28,11 +28,43 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func viewLogButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func intrustructionsHelpButtonHelped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case beltCoordinates_x:
+            beltCoordinates_y.becomeFirstResponder()
+        case beltCoordinates_y:
+            robotStartCoordinates_x.becomeFirstResponder()
+        case robotStartCoordinates_x:
+            robotStartCoordinates_y.becomeFirstResponder()
+        case robotStartCoordinates_y:
+            listOfCrates.becomeFirstResponder()
+        case listOfCrates:
+            listOfInstructions.becomeFirstResponder()
+        default:
+            beltCoordinates_x.resignFirstResponder()
+        }
+        
+        return true    }
+    
+}
